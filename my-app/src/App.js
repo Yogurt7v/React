ac74c1f1eb1676355aeccbab64e46bfd41d4943a
Yogurt7v <egorov-2k@yandex.ru>
2023-11-08 21:45:25 +0300

    without jsx fixed + done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import { createContext } from 'react';
+import React, { createContext, createElement } from 'react';
 
 export const App = () => {
-	const newDiv = document.createElement('div');
-	newDiv.className = 'App';
-
-	const newHeader = document.createElement('header');
-	newHeader.className = 'App-header';
-
-	const newImg = document.createElement('img');
-	newImg.src = logo;
-	newImg.className = 'App-logo';
-	newImg.alt = 'logo';
-
-	const newP = document.createElement('p');
-	newP.innerHTML = 'Lets code! <code>src/App.js</code> and save to reload.';
-
-	const newLink = document.createElement('a');
-	newLink.className = 'App-link';
-	newLink.href = 'https://reactjs.org';
-	newLink.target = '_blank';
-	newLink.rel = 'noopener noreferrer';
-	newLink.innerHTML = 'Learn React';
-
-	const newDate2 = new Date().getFullYear();
-	const secondP = document.createElement('p');
-	secondP.innerHTML = `${newDate2}`;
-
-	newDiv.append(newHeader);
-	newHeader.append(newImg, newP, newLink, secondP);
-	return document.body.append(newDiv);
+	const newDate = new Date().getFullYear();
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Lets code! <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 		</header>
+	// 	</div>
+	// );
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement('p', null, 'Lets code! src/App.js and save to reload.'),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('div', null, `${newDate}`),
+		),
+	);
 };
